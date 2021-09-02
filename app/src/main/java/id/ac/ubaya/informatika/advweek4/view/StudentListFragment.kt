dccb3b71ed6dca59458658aea840d7b5780c73a6
josestefanus <s160419044@student.ubaya.ac.id>
2021-09-02 16:05:21 +0700

    init2

---
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListFragment.kt
@@ -0,0 +1,27 @@
+package id.ac.ubaya.informatika.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.ubaya.informatika.advweek4.R
+import id.ac.ubaya.informatika.advweek4.viewmodel.ListViewModel
+
+
+class StudentListFragment : Fragment() {
+    private lateinit var viewModel:ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
