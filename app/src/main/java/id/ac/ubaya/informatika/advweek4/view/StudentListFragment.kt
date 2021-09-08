a3383529c21d796469395f8daef8cec3fa415bcf
josestefanus <s160419044@student.ubaya.ac.id>
2021-09-08 15:28:06 +0700

    init3

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.ac.ubaya.informatika.advweek4.R
 import id.ac.ubaya.informatika.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 
 class StudentListFragment : Fragment() {
@@ -23,5 +27,34 @@ class StudentListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = studentListAdapter
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                txtError.visibility = View.VISIBLE
+            } else {
+                txtError.visibility = View.GONE
+            }
+        })
+        viewModel.loadingDoneLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                recView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            } else {
+                recView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        })
+
     }
 }
