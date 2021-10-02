2071a655f109b96063de813b97e7663e52b1fb91
josestefanus <s160419044@student.ubaya.ac.id>
2021-10-02 15:31:13 +0700

    init5

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/util/Util.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/util/Util.kt
@@ -1,5 +1,9 @@
 package id.ac.ubaya.informatika.advweek4.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -7,6 +11,17 @@ import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import id.ac.ubaya.informatika.advweek4.R
 import java.lang.Exception
+
+fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
+        channel.setShowBadge(showBadge)
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+}
 
 fun ImageView.loadImage(url:String, progressBar:ProgressBar) {
     Picasso.get()
--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/MainActivity.kt
@@ -2,11 +2,58 @@ package id.ac.ubaya.informatika.advweek4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import id.ac.ubaya.informatika.advweek4.R
+import id.ac.ubaya.informatika.advweek4.util.createNotificationChannel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance:MainActivity ?= null
+
+        fun showNotification(title:String, content:String, icon:Int) {
+            val channelId = "${instance?.packageName}-${instance?.getString(R.string.app_name)}"
+            val builder = NotificationCompat.Builder(instance!!.applicationContext, channelId)
+                .apply {
+                    setSmallIcon(icon)
+                    setContentTitle(title)
+                    setContentText(content)
+                    setStyle(NotificationCompat.BigTextStyle())
+                    priority = NotificationCompat.PRIORITY_DEFAULT
+                    setAutoCancel(true)
+                }
+            val notif = NotificationManagerCompat.from(instance!!.applicationContext)
+            notif.notify(1001, builder.build())
+        }
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val observable = Observable.just("hellow","world", "!!")
+        //    .subscribeOn(Schedulers.io())
+        //    .observeOn(AndroidSchedulers.mainThread())
+        //    .subscribe(
+        //        { Log.d("Messages", "data captured: $it") },
+        //        { Log.e("Messages", "error: ${it.message.toString()}") },
+        //        { Log.d("Messages", "Completed") }
+        //    )
+
+        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT, true,
+            getString(R.string.app_name), "App channel")
+
     }
 }
